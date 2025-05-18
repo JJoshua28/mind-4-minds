af4cbd36c88fea11f562c03c323b776f5f27e9fa
Your Personal Name <your_personal_email@example.com>
2025-05-18 17:48:39 +0100

    adding routing to the application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {MentorSearchPageComponent} from "./modules/mentor-search/page/mentor-search-page/mentor-search-page.component";
+
 import {NavigationBarComponent} from "./shared/component/navigation-bar/navigation-bar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MentorSearchPageComponent, NavigationBarComponent],
+  imports: [RouterOutlet, NavigationBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import {MentorSearchPageComponent} from "./modules/mentor-search/page/mentor-search-page/mentor-search-page.component";
+import {InboxPageComponent} from "./modules/inbox/page/inbox-page/inbox-page.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: ``,
+    redirectTo: '/find-a-mentor',
+    pathMatch: 'full',
+  },
+  {
+    path: 'find-a-mentor',
+    component: MentorSearchPageComponent,
+    title: 'Find a mentor',
+  },
+  {
+    path: 'inbox',
+    component: InboxPageComponent,
+    title: 'Inbox',
+  }
+];
