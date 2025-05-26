114aa16a5a433db34436fbb8fee66231295a4e6b
Your Personal Name <your_personal_email@example.com>
2025-05-26 15:13:19 +0100

    Custom Text Input Form controls

---
+++ src/app/shared/component/text-input/text-input.component.ts
@@ -0,0 +1,48 @@
+import {Component, forwardRef, input, signal, WritableSignal} from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+
+@Component({
+  selector: 'app-text-input',
+  standalone: true,
+  imports: [],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => TextInputComponent),
+      multi: true,
+    },
+  ],
+  templateUrl: './text-input.component.html',
+  styleUrl: './text-input.component.scss'
+})
+export class TextInputComponent implements ControlValueAccessor {
+  label = input.required<string>();
+
+  value: WritableSignal<string> = signal("");
+
+  private onChange = (value: string) => {};
+  onTouched = () => {};
+
+  writeValue(value: string): void {
+    this.value.set(value || '');
+  }
+
+  registerOnChange(fn: (value: string) => void): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: () => void): void {
+    this.onTouched = fn;
+  }
+
+  setDisabledState?(isDisabled: boolean): void {
+    // optional: handle disabled state
+  }
+
+  onInput(value: string) {
+    this.value.set(value);
+    this.onChange(value);
+  }
+
+  protected readonly HTMLInputElement = HTMLInputElement;
+}
