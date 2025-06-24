a83ad1a0678ff9262f609361f947e7298a135f9e
Your Personal Name <your_personal_email@example.com>
2025-06-24 10:35:51 +0100

    navigation bar functionality

--- frontend/src/app/shared/component/navigation-bar/navigation-bar.component.ts
+++ frontend/src/app/shared/component/navigation-bar/navigation-bar.component.ts
@@ -1,6 +1,10 @@
-import {Component, inject, Signal, signal, WritableSignal} from '@angular/core';
+import {Component, inject, OnInit, Signal, signal, WritableSignal} from '@angular/core';
 import {NgClass} from "@angular/common";
 import {Router} from "@angular/router";
+import {UserService} from "../../services/user/user-service.service";
+import {UserType} from "../../../types/user-type.enum";
+import {map} from "rxjs";
+import {toSignal} from "@angular/core/rxjs-interop";
 
 enum NavigationItems {
   FIND_A_MENTOR="find a mentor",
@@ -8,30 +12,35 @@ enum NavigationItems {
   inbox='inbox',
   MY_MENTORS = "my mentors",
   profile='profile',
+  users='users',
 }
 
 enum SecondLevelNavigationItems {
   MY_MENTORS = "my mentors",
   MY_MENTEES = "my mentees",
   FIND_A_MENTOR="find a mentor",
-  inbox='inbox',
+  INBOX='inbox',
+  USERS = "users",
 }
 
 @Component({
   selector: 'app-navigation-bar',
   standalone: true,
-  imports: [
-    NgClass
+  providers: [
+    UserService,
   ],
   templateUrl: './navigation-bar.component.html',
   styleUrl: './navigation-bar.component.scss'
 })
 
-export class NavigationBarComponent {
+export class NavigationBarComponent implements OnInit {
   private _router = inject(Router);
 
+  private _userService = inject(UserService);
+
+  $roles!: Signal<UserType[]>;
   $hasNewInboxMessage: WritableSignal<boolean> = signal<boolean>(true);
-  $navigationItems: Signal<SecondLevelNavigationItems[]> = signal(Object.values(SecondLevelNavigationItems));
+  $navigationItems!: Signal<SecondLevelNavigationItems[]>;
 
   private routeMapper (navigationItem: NavigationItems): string {
     let navigationRoute = '';
@@ -52,10 +61,38 @@ export class NavigationBarComponent {
       case NavigationItems.MY_MENTEES:
         navigationRoute = 'my-mentees';
         break;
+      case NavigationItems.users:
+        navigationRoute = 'users';
+        break;
     }
 
     return navigationRoute
 
+  }
+
+  ngOnInit(): void {
+    this._userService.userDetails().pipe(map(user => user.roles))
+      .subscribe(userRoles => {
+        this.$roles = signal(userRoles);
+        this.$navigationItems = signal(this.generateRoles(this.$roles()));
+      })
+  }
+
+  generateRoles(roles: UserType[]) {
+    let navigationItems = Object.values(SecondLevelNavigationItems);
+    if(!roles.includes(UserType.MENTEE))
+    {
+      navigationItems = navigationItems.filter(item => item !== SecondLevelNavigationItems.FIND_A_MENTOR);
+      navigationItems = navigationItems.filter(item => item !== SecondLevelNavigationItems.MY_MENTORS);
+    }
+    if(!roles.includes(UserType.MENTOR)){
+      navigationItems = navigationItems.filter(item => item !== SecondLevelNavigationItems.MY_MENTEES);
+    }
+    if(!roles.includes(UserType.ADMIN)){
+      navigationItems = navigationItems.filter(item => item !== SecondLevelNavigationItems.USERS);
+    }
+
+    return navigationItems;
   }
 
   handleNavigateTo(navigationItem: string) {
