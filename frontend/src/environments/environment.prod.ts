16240b99bf65ccd753afdfdac80bd30f798d34b8
Your Personal Name <your_personal_email@example.com>
2025-07-10 17:53:24 +0100

    updating prod environment endpoint

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://mind-4-minds.web.app/api/',
+  apiUrl: 'http://34.252.27.219/api/',
   logo: '/assets/images/mind4minds%20logo.png',
   defaultProfilePic: '/static/assets/images/default.jpeg',
 };
