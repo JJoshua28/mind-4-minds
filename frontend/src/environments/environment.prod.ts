72c88ac2945ae2fe9d73dc8a83a6bf00c801963c
Your Personal Name <your_personal_email@example.com>
2025-07-14 10:39:25 +0100

    updating prod environment endpoint

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://34.252.27.219/api/',
+  apiUrl: 'https://mind-for-minds.duckdns.org/api/',
   logo: '/assets/images/mind4minds%20logo.png',
   defaultProfilePic: '/static/assets/images/default.jpeg',
 };
